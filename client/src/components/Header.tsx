8dab0da5c0ff9457501fb69750f11aa1263c66ae
dnshikhwan <danishikhwanneymar@gmail.com>
2025-02-06 20:35:09 +0300

    feat:remove notification bell

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -102,15 +102,6 @@ const Header = () => {
                   <p className="mr-4 text-gray-300 font-medium">
                     Credits : {credit}
                   </p>
-                  <button
-                    type="button"
-                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon aria-hidden="true" className="size-6" />
-                  </button>
-
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3">
                     <div>
