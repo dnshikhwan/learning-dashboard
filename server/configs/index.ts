9ff126ee8883a38b2ddf1ee59d87d88677c4f35f
dnshikhwan <danishikhwanneymar@gmail.com>
2025-02-07 20:25:28 +0300

    feat:fix cors error

--- server/configs/index.ts
+++ server/configs/index.ts
@@ -13,7 +13,10 @@ export const emailConfig = {
 };
 
 export const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_URL
+      : "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
